a6b2eb68e4e9d5d14b9411598b4800643edcb75a
algomachine007 <amaben202phantom@gmail.com>
2024-02-17 14:43:55 +0100

    adding pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,19 @@
 /** @type {import('next').NextConfig} */
+// Configuration object tells the next-pwa plugin
+const withPWA = require("next-pwa")({
+  dest: "public", // Destination directory for the PWA files
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker activation
+});
+
 const nextConfig = {
   experimental: {
     esmExternals: "loose",
     serverComponentsExternalPackages: ["mongoose"],
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
   },
 
   // webpack: (config) => {
@@ -13,4 +24,5 @@ const nextConfig = {
   // },
 };
 
-module.exports = nextConfig;
+// Export the combined configuration for Next.js with PWA support
+module.exports = withPWA(nextConfig);
