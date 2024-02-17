3505e5c10f4000893cb2e22408c5f99814ab8ef2
algomachine007 <amaben202phantom@gmail.com>
2024-02-17 15:36:18 +0100

    manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,10 +34,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
-      />
       <body className={inter.className}>
         <ToastContainer position="top-center" />
 
